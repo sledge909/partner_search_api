ae4d67bb0a23dd35599266a22139b3eded4db7c7
Scott Wenborne <scott.wenborne@onthebeach.co.uk>
2021-03-12 16:45:12 +0000

    Create the PartnerSearchService

--- app/controllers/api/v1/partner_searches_controller.rb
+++ app/controllers/api/v1/partner_searches_controller.rb
@@ -1,7 +1,10 @@
 class Api::V1::PartnerSearchesController < ApplicationController
+  # include ActionController::HttpAuthentication::Basic::ControllerMethods
+  # http_basic_authenticate_with name: ENV['API_USER'],
+                               # password: ENV['API_PASSWORD']
 
   def index
-
+    render json: PartnerSearchService.search(params)
   end
 
   def show
