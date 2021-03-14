e23a3c01d017229049b38a5038ad0ad70adbbd97
Scott Wenborne <scott.wenborne@onthebeach.co.uk>
2021-03-14 08:50:32 +0000

    Add dependent destroy to relationships

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,7 +1,7 @@
 class Partner < ApplicationRecord
-  has_many :partner_services
+  has_many :partner_services, dependent: :destroy
   has_many :services, through: :partner_services
-  has_many :partner_skills
+  has_many :partner_skills, dependent: :destroy
   has_many :skills, through: :partner_skills
 
   def self.by_service(param)
--- app/models/service.rb
+++ app/models/service.rb
@@ -1,5 +1,5 @@
 class Service < ApplicationRecord
   has_many :partner_services
   has_many :partners, through: :partner_services
-  has_many :skills
+  has_many :skills, dependent: :destroy
 end
